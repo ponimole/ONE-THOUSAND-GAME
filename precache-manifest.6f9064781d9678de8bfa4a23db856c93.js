fb68cdf0cd389042863012bee83f5a683a9a8490
ponimole <onimolep@yahoo.com>
2020-03-23 15:39:53 -0500

    Updates

---
+++ precache-manifest.6f9064781d9678de8bfa4a23db856c93.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2380f7464cbc284c7445551cde807241",
+    "url": "/ONE-THOUSAND-GAME/index.html"
+  },
+  {
+    "revision": "dcf9923dcb5a1d1e5845",
+    "url": "/ONE-THOUSAND-GAME/static/css/main.789e67fa.chunk.css"
+  },
+  {
+    "revision": "bc393de64d682403eaad",
+    "url": "/ONE-THOUSAND-GAME/static/js/2.34c7f0ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ONE-THOUSAND-GAME/static/js/2.34c7f0ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf9923dcb5a1d1e5845",
+    "url": "/ONE-THOUSAND-GAME/static/js/main.dde2b02d.chunk.js"
+  },
+  {
+    "revision": "1a6243e0b1a982f36536",
+    "url": "/ONE-THOUSAND-GAME/static/js/runtime-main.27600db1.js"
+  },
+  {
+    "revision": "7194cb72d404543549a2cb88270e0edc",
+    "url": "/ONE-THOUSAND-GAME/static/media/GREicon.7194cb72.png"
+  },
+  {
+    "revision": "ec0f1ff2a365c46bb3d63355b834b9dc",
+    "url": "/ONE-THOUSAND-GAME/static/media/Screenshot1.ec0f1ff2.png"
+  },
+  {
+    "revision": "2f5f1df1435ba6d6f52321493e60874a",
+    "url": "/ONE-THOUSAND-GAME/static/media/Screenshot2.2f5f1df1.png"
+  },
+  {
+    "revision": "fc18a81e57f5779a6e20e4f56625d3fc",
+    "url": "/ONE-THOUSAND-GAME/static/media/Screenshot3.fc18a81e.png"
+  }
+]);
